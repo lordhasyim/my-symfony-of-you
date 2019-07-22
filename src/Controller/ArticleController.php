b8024280c90d177345c6e90135b4d87d5fb2d892
lordhasyim <lordhasyim@gmail.com>
2019-07-22 11:50:50 +0700

    add twig template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -23,7 +24,10 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf("Future page %s", $slug));
+        //return new Response(sprintf("Future page %s", $slug));
+        return $this->render('article/show.html.twig',[
+            'title' =>  ucwords(str_replace('-', ' ', $slug))
+        ]);
     }
     
 
