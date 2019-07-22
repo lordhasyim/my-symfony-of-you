8d3e328fde2b6d9741a7a0ee954ffad7c521df9e
lordhasyim <lordhasyim@gmail.com>
2019-07-22 14:41:52 +0700

    add article like feature

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,31 +4,63 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      * @return Response
      */
     public function homepage()
     {
-        return new Response('this is my first response');
+        //return new Response('this is my first response');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    //public function show($slug)
+    public function show($slug, Environment $twigEnvironment) // call twig manually
     {
+        $comments = [
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry, has been the industry\'s standard dummy text ever since the 1500s',
+            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. isn\'t?',
+            ' arema sam'
+        ];
         //return new Response(sprintf("Future page %s", $slug));
-        return $this->render('article/show.html.twig',[
-            'title' =>  ucwords(str_replace('-', ' ', $slug))
-        ]);
+//        return $this->render('article/show.html.twig',[
+//            'title' =>  ucwords(str_replace('-', ' ', $slug)),
+//            'slug' => $slug,
+//            'comments' => $comments
+//        ]);
+
+        $html = $twigEnvironment->render('article/show.html.twig',[
+             'title' =>  ucwords(str_replace('-', ' ', $slug)),
+             'slug' => $slug,
+             'comments' => $comments
+         ]);
+
+        return new Response($html);
     }
-    
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        //$this->json is equal to new JsonResponse();
+
+        $logger->info("article being harted");
+
+        return $this->json(['hearts' => rand(5, 100)]);
+    }
+
+
 
 }
